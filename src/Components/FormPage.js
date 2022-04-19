13f2267a566fa7182d3965479d283a733dc7b689
ybuchi <yabuch_ch@hotmail.com>
2022-04-18 21:59:11 -0400

    Created the two forms and am dynimcally showing them

--- src/Components/FormPage.js
+++ src/Components/FormPage.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import NewLeisureActivityForm from "./NewLeisureActivityForm";
+import OutpostActivityForm from "./OutpostActivityForm";
+import Button from 'react-bootstrap/Button'
+import "./Form.css"
 
 function FormPage(){
+    // const [formIsVisible, setFormIsVisible] = useState(false)
+    const [formType, setFormType] = useState("")
+
+    function handleFormAppear(e){
+        console.log(e.target.name)
+        setFormType(e.target.name)
+        // setFormType
+    }
     return(
         <>
             <h1 style={{textAlign: "center", padding: "30px 40px"}}>Are you doing green things? Show it here!</h1>
-            <NewLeisureActivityForm />
+
+            <Button className="centered" name="outpost" onClick={handleFormAppear}>Outpost Form</Button>
+            <Button className="centered" name="leisure" onClick={handleFormAppear}>Leisure Form</Button>
+
+            { formType === "outpost" ? <OutpostActivityForm/> : <NewLeisureActivityForm/>}
         </>
     )
 }
---
+++ src/Components/OutpostActivityForm.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import "./NewLeisureActivityForm.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function OutpostActivityForm(){
+    const [outpostActivityForm, setOutpostActivityFormState] = useState({
+        pseudonym: "",
+        activity_type: "Picnic at the Park",
+        activity_location: "",
+        activity_description: "",
+        activity_date: Date.now(),
+        comment: ""
+      })
+
+      const {pseudonym, 
+             activity_type, 
+             activity_location, 
+             activity_description, 
+             activity_date, 
+             comment} = outpostActivityForm
+
+      function handleOutpostActivityFormChange(e){
+        const new_value = e.target.value;
+        console.log(e.target.name)
+        setOutpostActivityFormState({...outpostActivityForm, [e.target.name]: new_value })
+      }
+    return(
+        <Container fluid>
+        <Form className="new-leasure-form">
+            <Form.Group>
+                <Form.Label>Pseudonym</Form.Label>
+                <Form.Control name="pseudonym" type="text" value={pseudonym} onChange={handleOutpostActivityFormChange}/>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Activity Type</Form.Label>
+                <Form.Select name="activity_type" value={activity_type} size="text" onChange={handleOutpostActivityFormChange}>
+                    <option>Recylcing</option>
+                    <option>Composting</option>
+                    <option>Frisbee</option>
+                    <option>Pickup Soccer</option>
+                    <option>Other Outdoor Sports</option>
+                </Form.Select>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Activity Location</Form.Label>
+                <Form.Control name="activity_location" value={activity_location} onChange={handleOutpostActivityFormChange} type="text"/>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Activity Description</Form.Label>
+                <Form.Control name="activity_description" value={activity_description} onChange={handleOutpostActivityFormChange} type="text"/>
+                <Form.Text>Please limit text to no more than N characters.</Form.Text>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Activity Date</Form.Label>
+                <Form.Control name="activity_date" value={activity_date} onChange={handleOutpostActivityFormChange} type="date"/>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Comment</Form.Label>
+                <Form.Control id="outpost-comment-box" name="comment" value={comment} onChange={handleOutpostActivityFormChange} type="text"/>
+            </Form.Group>
+
+            {/* TO DO: Handle on Submit to make a fetch request */}
+            <Button style={{margin: "20px 0px", backgroundColor: "#F4A442", border:"none"}} type="submit">Submit</Button>
+
+        </Form>
+        </Container>
+    )
+}
+
+export default OutpostActivityForm;
