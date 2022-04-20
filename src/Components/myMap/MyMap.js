dc0d1ccf0f154af4af270189cad02958b38b5b72
Xhulio Mihali <mihalijulio@gmail.com>
2022-04-20 13:15:53 -0400

    added tooltip to map

--- src/Components/myMap/MyMap.js
+++ src/Components/myMap/MyMap.js
@@ -1,18 +1,19 @@
 import React,{useState} from "react"
 import { Map, Marker, Overlay } from "pigeon-maps"
+import './mymap.css'
 
 export default function MyMap({outpost}) {
 
   // console.log(outpost.ntaname)
-
   const [overlay,setOverlay] =useState(false)
   const [overCoordinates, setOverCoordinates] = useState([40.74253751019931, -73.99234182490424])
   const [image,setImage] = useState('https://i.pinimg.com/564x/42/7d/74/427d74978d39aa990dafa5fdffe8598d--monsters-inc-go-green.jpg')
+ 
 
   const handleClick = (e) => {
     let coor = e.anchor
     setOverlay(true)
-    console.log(e)
+    console.log(e.style)
     setImage(`${coor}`)
     setOverCoordinates(coor)
   }
@@ -22,12 +23,15 @@ export default function MyMap({outpost}) {
   }
 
   const displayOverlay = (
-    <Overlay anchor={overCoordinates} offset={[120, 79]}>
-      <img src={image} width={240} height={158} alt={image} />
+    <Overlay anchor={overCoordinates} offset={[0, 0]}>
+        <div id="tooltip">
+          <h5 class="card-title">{`${overCoordinates}`}</h5>
+          <p class="card-text">Here are some comments</p>
+        </div>
     </Overlay>
   )
 
-  const displayMarker = outpost.map(out => <Marker key={out.id} onClick={handleClick} width={50} anchor={[parseFloat(out.latitude),parseFloat(out.longitude)]} />)
+  const displayMarker = outpost.map(out => <Marker key={out.id} onClick={handleClick} width={50} anchor={[parseFloat(out.latitude),parseFloat(out.longitude)]} color={'#168513'}  />)
 
   return (
     <Map onClick={handleTooltip} height={1000} defaultCenter={overCoordinates} defaultZoom={14}>
