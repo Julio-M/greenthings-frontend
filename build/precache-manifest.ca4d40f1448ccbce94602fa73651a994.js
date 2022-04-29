6406ba187b0f7b70e25992459b196a491368539e
Xhulio Mihali <mihalijulio@gmail.com>
2022-04-29 13:31:25 -0400

    build folder

---
+++ build/precache-manifest.ca4d40f1448ccbce94602fa73651a994.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d02f8942a76c633f290",
+    "url": "/static/css/main.c12322e5.chunk.css"
+  },
+  {
+    "revision": "9d02f8942a76c633f290",
+    "url": "/static/js/main.9d02f894.chunk.js"
+  },
+  {
+    "revision": "e26974a781e160b516d0",
+    "url": "/static/js/1.e26974a7.chunk.js"
+  },
+  {
+    "revision": "f6011789f6be91dcb522",
+    "url": "/static/css/2.4f908731.chunk.css"
+  },
+  {
+    "revision": "f6011789f6be91dcb522",
+    "url": "/static/js/2.f6011789.chunk.js"
+  },
+  {
+    "revision": "2adb094baa481733d756",
+    "url": "/static/js/runtime~main.2adb094b.js"
+  },
+  {
+    "revision": "e6c7e3704b08f1dea7cf304d4176fde7",
+    "url": "/static/media/logo.e6c7e370.png"
+  },
+  {
+    "revision": "6f840db7f90c06ea9f02acb3f2d51e30",
+    "url": "/static/media/deer.6f840db7.png"
+  },
+  {
+    "revision": "1a0f3b3515dce559905f2642495b27b7",
+    "url": "/static/media/squirrel.1a0f3b35.png"
+  },
+  {
+    "revision": "0737426108c1a4a9764196f4d2d1c8a0",
+    "url": "/static/media/falcon.07374261.png"
+  },
+  {
+    "revision": "c278b1cb23525e07ffb0de54c50fcbe1",
+    "url": "/static/media/pigeon.c278b1cb.png"
+  },
+  {
+    "revision": "a71720fb5995742c655f2719edc98fa2",
+    "url": "/static/media/rat.a71720fb.png"
+  },
+  {
+    "revision": "523493fd446d76c12d33c69f75a71fea",
+    "url": "/static/media/coyote.523493fd.png"
+  },
+  {
+    "revision": "35facfec7e39730c06ae1f11f2b7ae99",
+    "url": "/static/media/raccoon.35facfec.png"
+  },
+  {
+    "revision": "fcc2a1c9a0d3fd9c8292901062aae8a8",
+    "url": "/static/media/map.fcc2a1c9.png"
+  },
+  {
+    "revision": "f3a6065b9d00a3c5993a2883cca1c00a",
+    "url": "/static/media/activity.f3a6065b.png"
+  },
+  {
+    "revision": "77a6fdbc5b3a16d3105755696c271a01",
+    "url": "/static/media/park.77a6fdbc.png"
+  },
+  {
+    "revision": "133f5105734af0e9b519873b10f8d745",
+    "url": "/index.html"
+  }
+];
